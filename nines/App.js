40c76b44c6b6d5d2cddaed5a04bd7e95aaf6381f
alexclemente1985 <alexclemente1985@gmail.com>
2020-03-12 18:35:58 -0300

    backup

--- nines/App.js
+++ nines/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Field from './src/components/field';
+import Field from './src/components/Field';
 import params from './src/params';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando o Mines!!!</Text>
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instructions}>
-          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+          Tamanho da grade: {params.getRowsAmount()}X{params.getColumnsAmount()}
         </Text>
         <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={4} />
-        <Field opened nearMines={5} />
-        <Field opened nearMines={6} />
       </View>
     );
   }
@@ -36,4 +29,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  instructions: {},
 });
